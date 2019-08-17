6f097d5e55a8d32f448365abc8398191019483cc
Paul Ambrose <pambrose@mac.com>
2019-08-16 17:49:47 -0800

    Add cancel example

---
+++ src/main/kotlin/org/athenian/Cancels2.kt
@@ -0,0 +1,24 @@
+package org.athenian
+
+import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+        val job =
+            launch {
+                log("Coroutine start")
+                launch {
+                    log("Child coroutine start")
+                    try {
+                        delay(Long.MAX_VALUE)
+                    } catch (e: CancellationException) {
+                        log("Coroutine cancelled - ${e.message}")
+                        throw e
+                    }
+                }
+            }
+        delay(1000)
+        job.cancelAndJoin()
+        log("Done")
+    }
+}
