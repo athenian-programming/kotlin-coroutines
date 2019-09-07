b10102aae7a0fc6260ac76e80803e118ffe8299d
Paul Ambrose <pambrose@mac.com>
2019-09-07 14:34:14 -0700

    Rename classes

--- src/main/kotlin/org/athenian/Scopes1.kt
+++ src/main/kotlin/org/athenian/Scopes1.kt
@@ -1,41 +1,39 @@
 package org.athenian
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.coroutineContext
 import kotlin.time.ExperimentalTime
-import kotlin.time.seconds
-
-// See https://medium.com/@elizarov/explicit-concurrency-67a8e8fd9b25
+import kotlin.time.milliseconds
 
 @ExperimentalTime
 fun main() {
     runBlocking {
-        launchingCall()
-        delayedCall1()
+
+        launch { scopeCheck(this) }
+
+
+        launch {
+            delay(200.milliseconds)
+            log("Task from runBlocking")
+        }
+
+        coroutineScope {
+            launch {
+                delay(500.milliseconds)
+                log("Task from nested launch")
+            }
+
+            delay(100.milliseconds)
+            log("Task from coroutine scope")
+        }
+
+        log("Coroutine scope is over")
     }
 }
 
-// Declaring this as a CoroutineScope extension function enables an embedded call to launch()
-@ExperimentalTime
-fun CoroutineScope.launchingCall() {
-    log("Starting launchingCall()")
-    launch {
-        delayedCall2()
-    }
-    log("Ending launchingCall()")
-}
-
-@ExperimentalTime
-suspend fun delayedCall1() {
-    log("Starting delayedCall1()")
-    delay(1.seconds)
-    log("Ending delayedCall1()")
-}
-
-@ExperimentalTime
-suspend fun delayedCall2() {
-    log("Starting delayedCall2()")
-    delay(1.seconds)
-    log("Ending delayedCall2()")
+suspend fun scopeCheck(scope: CoroutineScope) {
+    log("coroutineContext are equal: ${scope.coroutineContext === coroutineContext}")
 }
---
+++ src/main/kotlin/org/athenian/Scopes2.kt
@@ -0,0 +1,41 @@
+package org.athenian
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
+
+// See https://medium.com/@elizarov/explicit-concurrency-67a8e8fd9b25
+
+@ExperimentalTime
+fun main() {
+    runBlocking {
+        launchingCall()
+        delayedCall1()
+    }
+}
+
+// Declaring this as a CoroutineScope extension function enables an embedded call to launch()
+@ExperimentalTime
+fun CoroutineScope.launchingCall() {
+    log("Starting launchingCall()")
+    launch {
+        delayedCall2()
+    }
+    log("Ending launchingCall()")
+}
+
+@ExperimentalTime
+suspend fun delayedCall1() {
+    log("Starting delayedCall1()")
+    delay(1.seconds)
+    log("Ending delayedCall1()")
+}
+
+@ExperimentalTime
+suspend fun delayedCall2() {
+    log("Starting delayedCall2()")
+    delay(1.seconds)
+    log("Ending delayedCall2()")
+}
