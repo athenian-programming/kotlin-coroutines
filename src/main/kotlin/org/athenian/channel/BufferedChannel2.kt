44490d5b3e0f38e15c437dec0177a12433a0b227
Paul Ambrose <pambrose@mac.com>
2019-11-18 23:01:53 -0800

    Add second version of BufferedChannel

---
+++ src/main/kotlin/org/athenian/channel/BufferedChannel2.kt
@@ -0,0 +1,27 @@
+package org.athenian.channel
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.athenian.delay
+import kotlin.time.milliseconds
+
+fun main() {
+    val iterations = 10
+    runBlocking {
+        val channel = Channel<Int>(Channel.UNLIMITED)
+
+        launch {
+            repeat(iterations) {
+                println("Writing $it")
+                channel.send(it)
+            }
+            channel.close()
+        }
+
+        for (i in channel) {
+            println("Reading $i")
+            delay(1_000.milliseconds)
+        }
+    }
+}
