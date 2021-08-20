6b634889f411bf833d3d590965f506bfcf88c190
pambrose <pambrose@mac.com>
2021-08-20 11:48:09 -0700

    Update code

--- src/main/kotlin/org/athenian/examples/WcCommand.kt
+++ src/main/kotlin/org/athenian/examples/WcCommand.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.io.IOException
-import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -30,48 +29,54 @@ fun main() {
 
   data class Contents(val words: Int, val chars: Int)
 
+  val DONE = Contents(-1, -1)
+
   val charCount = AtomicInteger(0)
   val wordCount = AtomicInteger(0)
   val lineCount = AtomicInteger(0)
 
   runBlocking {
     val workerCount = 5
-    val latch = CountDownLatch(workerCount)
     val fnameChannel = Channel<String>()
     val calcChannel = Channel<Contents>()
 
     launch {
+      var count = 0
       for (calc in calcChannel) {
-        charCount.addAndGet(calc.chars)
-        wordCount.addAndGet(calc.words)
-        lineCount.incrementAndGet()
+        if (calc == DONE) {
+          count++
+          if (count == workerCount)
+            calcChannel.close()
+        } else {
+          charCount.addAndGet(calc.chars)
+          wordCount.addAndGet(calc.words)
+          lineCount.incrementAndGet()
+        }
       }
     }
 
     repeat(workerCount) {
       launch {
         for (filename in fnameChannel) {
-          println("$it $filename")
+          //println("$it $filename")
           if (filename.endsWith(".txt")) {
             File(filename).inputStream().bufferedReader().useLines { lines ->
               lines.forEach { line ->
-                calcChannel.send(Contents(line.split("\\s+".toRegex()).count(), line.length + 1))
+                val words = line.split("\\s+".toRegex()).count()
+                val chars = line.length + 1
+                calcChannel.send(Contents(words, chars))
               }
             }
           }
         }
-        println("Finished wc worker")
-        latch.countDown()
-        if (latch.count == 0L)
-          calcChannel.close()
+        calcChannel.send(DONE)
       }
     }
 
     launch {
-      println("Running command")
       runCommand("ls")
         .forEach { line ->
-          println("Sending")
+          println("Sending $line")
           fnameChannel.send(line)
         }
       fnameChannel.close()
